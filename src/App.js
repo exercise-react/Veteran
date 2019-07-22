aa600b93209d679e84d9261dd51789640f3a196f
Yuriy <yury.nadolsky@itmint.ca>
2019-07-22 14:12:34 +0300

    fix theme

--- src/App.js
+++ src/App.js
@@ -2,18 +2,27 @@ import React from 'react';
 import Header from './components/Header';
 import SideNav from './containers/SideNav';
 import Main from './components/Main';
-
+import theme from './theme';
 import './App.css';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import {BrowserRouter} from "react-router-dom";
+import { ThemeProvider } from '@material-ui/styles';
+
 
 function App() {
     return (
-        <div className='head-container'>
-            <Header className='header-container'/>
-            <div className='content-container'>
-                <SideNav className='sidenav-container'/>
-                <Main className='main-container'/>
-            </div>
-        </div>
+        <BrowserRouter>
+            <ThemeProvider  theme={theme}>
+                <CssBaseline/>
+                <div className='head-container'>
+                    <Header className='header-container'/>
+                    <div className='content-container'>
+                        <SideNav className='sidenav-container'/>
+                        <Main className='main-container'/>
+                    </div>
+                </div>
+            </ThemeProvider >
+        </BrowserRouter>
     );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,10 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import {ThemeProvider} from '@material-ui/styles';
-import theme from './theme';
-import {BrowserRouter} from "react-router-dom";
 import {Provider} from 'react-redux';
 import {applyMiddleware, createStore} from "redux";
 import thunk from "redux-thunk";
@@ -22,17 +18,9 @@ const store = createStore(rootReducer, applyMiddleware(...middleware));
 
 
 ReactDOM.render(
-    <>
-
-        <ThemeProvider theme={theme}>
-            <CssBaseline/>
-            <BrowserRouter>
-                <Provider store={store}>
-                    <App/>
-                </Provider>
-            </BrowserRouter>
-        </ThemeProvider>
-    </>, document.getElementById('root'));
+    <Provider store={store}>
+        <App/>
+    </Provider>, document.getElementById('root'));
 
 
 // If you want your app to work offline and load faster, you can change
