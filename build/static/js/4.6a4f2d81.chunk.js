ee92d98d214d0538ac5d9e6570b003ac1d15583b
Yuriy <yury.nadolsky@itmint.ca>
2019-07-24 18:04:02 +0300

    builds

---
+++ build/static/js/4.6a4f2d81.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{233:function(e,t,n){"use strict";n.r(t);var a=n(0),l=n.n(a);t.default=function(){return l.a.createElement("div",{className:"home-container"},l.a.createElement("div",{className:"text-bold"},"Welcome !"),l.a.createElement("br",null),l.a.createElement("div",null,"Please select 'Veteran Information' in the navigation to the left to see the Client form."))}}}]);
+//# sourceMappingURL=4.6a4f2d81.chunk.js.map
